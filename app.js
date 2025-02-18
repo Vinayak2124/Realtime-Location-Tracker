9fcde1dd3dd2222a03d17f8b7c060853b09f09fa
Vinayak Chaube <vinayakchaube24@gmail.com>
2025-02-18 23:31:35 +0530

    Updated location tracking with colored markers

--- app.js
+++ app.js
@@ -8,14 +8,31 @@ const socketio = require('socket.io');
 const server = http.createServer(app)
 const io = socketio(server);
 
+
+let users = {};
+const markerColors = ["red", "blue", "green", "orange", "purple","yellow"];
 io.on('connection', function (socket) {
     console.log("connected");
-    socket.on("send-location", function (data){
-        io.emit("receive-location", { id: socket.id, ...data });
-    })
-    socket.on("disconnect", function () {
-        io.emit("user-disconnect", socket.id)
-    })
+
+    socket.on("join", (name) => {
+         const randomColor = markerColors[Math.floor(Math.random() * markerColors.length)];
+       users[socket.id] = { id: socket.id, name, latitude: 0, longitude: 0, color: randomColor };
+        socket.emit("all-users", users);  // Send all existing users to new user
+    });
+
+
+    
+  socket.on("send-location", (data) => {
+        if (users[socket.id]) {
+            users[socket.id] = { ...users[socket.id], ...data };
+            io.emit("receive-location", users[socket.id]); // Broadcast to everyone
+        }
+    });
+    socket.on("disconnect", () => {
+        console.log("User disconnected:", socket.id);
+        io.emit("user-disconnect", socket.id);
+        delete users[socket.id]; // Remove from list
+    });
     
 });
 app.set("view engine", "ejs")
--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,8 @@
 const socket = io();
 
 
+
+
 if (navigator.geolocation) {
     navigator.geolocation.watchPosition((position) => {
         const { latitude, longitude } = position.coords;
@@ -23,15 +25,45 @@ L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 
 const markers = {};
 
-socket.on("receive-location", (data) => {
-    const { id, latitude, longitude } = data;
-    map.setView([latitude, longitude], 100);
-    if (markers[id]) {
-        markers[id].setLatLng([latitude, longitude]);
-    }else {
-        markers[id] = L.marker([latitude,longitude]).addTo(map)
+
+const userName = prompt("Enter your name:");
+socket.emit("join", userName);
+
+function createCustomIcon(color) {
+    return L.icon({
+        iconUrl: `https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-${color}.png`,
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-shadow.png",
+        shadowSize: [41, 41]
+    });
+}
+
+function addOrUpdateMarker(id,name, latitude, longitude,color) {
+     if (markers[id]) {
+        markers[id].setLatLng([latitude, longitude]).bindPopup(name).openPopup();
+    } else {
+        const icon = createCustomIcon(color);
+        markers[id] = L.marker([latitude, longitude], { icon })
+            .addTo(map)
+            .bindPopup(name) // Show user’s name below the marker
+            .openPopup();
     }
-})
+}
+
+socket.on("all-users", (users) => {
+    Object.values(users).forEach(user => {
+        addOrUpdateMarker(user.id, user.name ,user.latitude, user.longitude, user.color);
+    });
+});
+
+
+
+
+socket.on("receive-location", (data) => {
+    addOrUpdateMarker(data.id, data.name, data.latitude, data.longitude,data.color);
+});
 
 socket.on("user-disconnect", (id) => {
     if (markers[id]) {
